0f901f5a21c50a219fe1dbaef894f30adaca9832
j2nhyeok <103464891+j2nhyeok@users.noreply.github.com>
2022-06-29 11:54:37 +0900

    Add files via upload
    최진혁 파일임.    내부 기능 구현 하는 중

---
+++ team lotto/src/LottoNumber.java
@@ -0,0 +1,73 @@
+import java.util.List;
+
+public class LottoNumber {
+
+	public int numbers[] = new int[6];
+	private String category;
+	private Character alphabet;
+	
+	public LottoNumber(char alphabet, String category, List<Integer> numbers) {
+		this.alphabet = alphabet;
+		this.category = category;
+		for(int i = 0; i <=5 ; i++) {
+			this.numbers[i] = numbers.get(i);
+		}
+	}
+	
+	
+	public Character getAlphabet() {
+		return alphabet;
+	}
+
+
+
+
+
+	public void setAlphabet(Character alphabet) {
+		this.alphabet = alphabet;
+	}
+
+
+
+
+
+	public String getNumbers() {
+		StringBuilder sb = new StringBuilder();
+		for(int a : numbers) {
+			sb.append(a).append(" ");
+		}
+		return sb.toString();
+	}
+
+
+
+
+
+	public String getCategory() {
+		return category;
+	}
+
+
+
+
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+
+
+
+
+	public void setNumbers(int[] numbers) {
+		this.numbers = numbers;
+	}
+
+
+
+
+
+	
+	
+	
+}
